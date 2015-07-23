4536011124e2154bea98adff77f072b3f5504da4
Ricky Reusser <rsreusser@gmail.com>
2015-07-22 22:01:56 -0400

    Documentation!

--- test/gulp-markdown-equations.js
+++ test/gulp-markdown-equations.js
@@ -8,9 +8,9 @@ var test = require('tape')
 
 //test.createStream().pipe(require('tap-spec')()).pipe(process.stdout);
 
-function testBehavior(input, defaults, eqCallback, mdCallback) {
+function testBehavior(input, options, eqCallback, mdCallback) {
 
-  var mdeq = gulpMarkdownEquations({defaults: defaults})
+  var mdeq = gulpMarkdownEquations(options)
 
   mdeq.write(new File({ path: 'sample.mdtex', contents: input }))
   mdeq.end()
@@ -18,9 +18,13 @@ function testBehavior(input, defaults, eqCallback, mdCallback) {
   mdeq.pipe(tap(function(file) {
     if( file.contents === null ) return
     if( file.basename.match(/\.md$/) ) {
-      mdCallback.bind(file)()
+      if( mdCallback ) mdCallback.bind(file)()
     } else {
-      mdeq.complete(file,eqCallback)
+      if( eqCallback ) {
+        mdeq.complete(file,eqCallback)
+      } else {
+        mdeq.complete(file,function(cb) { cb() })
+      }
     }
   }))
 
@@ -28,42 +32,28 @@ function testBehavior(input, defaults, eqCallback, mdCallback) {
 }
 
 
+test('gulp-markdown-equations: a null templator passes tex through',function(t) {
+  testBehavior(fixtures['sample.mdtex'], { templator: null }, function(cb) {
+    t.equal( this.foo, 'bar', 'gets preprocessed config' )
+    t.equal( this.equation.templated, 'y = \\frac{1}{x}', 'tex is not templated' )
+    cb('test')
+  }).on('end',t.end)
+})
 
-test('transforms a file',function(t) {
+test('gulp-markdown-equations: a null preprocessor skips params',function(t) {
+  testBehavior(fixtures['sample.mdtex'], { preprocessor: null }, function(cb) {
+    t.equal( this.foo, undefined, 'params are not parsed' )
+    cb('test')
+  }).on('end',t.end)
+})
 
-  var numEq = 0, numMd = 0, md
-
-  var defaults = {
-    inline: { beep: 'boop' },
-    display: { baz: 'bop' }
-  }
-
-  testBehavior( fixtures['sample.mdtex'], defaults, function(cb) {
+test('gulp-markdown-equations: number of files is correct',function(t) {
+  var numEq = 0, numMd = 0
+  testBehavior( fixtures['sample.mdtex'], {}, function(cb) {
     numEq++
-
-    t.assert( this.equation.basename.match(/^y-frac1x-[a-z0-9]{10}.tex/), 'has the correct filename')
-    t.equal( this.equation.tex, 'y = \\frac{1}{x}', 'has the tex extracted')
-    t.equal( this.foo, 'bar', 'parsed and received its params')
-    t.assert( this.equation.templated.match(/^\\documentclass/), 'got inserted into the latex template')
-
-    if( this.display ) {
-      t.equal(this.baz, 'bop', 'receives default display mode config')
-    } else {
-      t.equal(this.beep, 'boop', 'receives default display mode config')
-    }
-
-    // Complete the transformation:
-    cb('equation-number-' + numEq)
-
+    cb()
   }, function() {
     numMd++
-
-    // Markdown file:
-    md = this.contents.toString()
-
-    t.assert( md.match(/equation-number-1/), 'contains the first equation')
-    t.assert( md.match(/equation-number-2/), 'contains the second equation')
-
   }).on('end',function() {
     t.equal(numEq, 2, 'got two equations')
     t.equal(numMd, 1, 'got one markdown file')
@@ -72,7 +62,95 @@ test('transforms a file',function(t) {
 
 })
 
+test('gulp-markdown-equations: default options get passed',function(t) {
 
+  var options = {
+    defaults: {
+      inline: { beep: 'boop' },
+      display: { baz: 'bop' }
+    }
+  }
+
+  testBehavior( fixtures['sample.mdtex'], options, function(cb) {
+    if( this.display ) {
+      t.equal(this.baz, 'bop', 'receives default display mode config')
+    } else {
+      t.equal(this.beep, 'boop', 'receives default display mode config')
+    }
+    cb()
+  }).on('end',t.end)
+})
+
+test('gulp-markdown-equations: correct result is inserted',function(t) {
+  var numEq = 0, md
+  testBehavior( fixtures['sample.mdtex'], {}, function(cb) {
+    numEq++
+    cb('equation-number-' + numEq)
+  },function() {
+    md = this.contents.toString()
+    t.assert( md.match(/equation-number-1/), 'markdown contains the result of the first equation')
+    t.assert( md.match(/equation-number-2/), 'markdown contains the result of the second equation')
+  }).on('end',t.end)
+})
+
+test('gulp-markdown-equations: has the correct filename',function(t) {
+  testBehavior( fixtures['sample.mdtex'], {}, function(cb) {
+    t.assert( this.equation.basename.match(/^y-frac1x-[a-z0-9]{10}.tex/), 'has the correct filename')
+    cb()
+  }).on('end',t.end)
+})
+
+test('gulp-markdown-equations: tex is extracted',function(t) {
+  testBehavior( fixtures['sample.mdtex'], {}, function(cb) {
+    t.equal( this.equation.tex, 'y = \\frac{1}{x}', 'has the tex extracted')
+    cb()
+  }).on('end',t.end)
+})
+test('gulp-markdown-equations: preprocesses and inserts params',function(t) {
+  testBehavior( fixtures['sample.mdtex'], {}, function(cb) {
+    t.equal( this.foo, 'bar', 'parsed and received its params')
+    cb()
+  }).on('end',t.end)
+})
+
+test('gulp-markdown-equations: latex templating',function(t) {
+  testBehavior( fixtures['sample.mdtex'], {}, function(cb) {
+    t.assert( this.equation.templated.match(/^\\documentclass/), 'got inserted into the latex template')
+    cb()
+  }).on('end',t.end)
+})
+
+test('gulp-markdown-equations: fails if callback executed more than once',function(t) {
+  testBehavior( fixtures['sample.mdtex'], {}, function(cb) {
+    cb()
+    t.throws(cb, Error, 'throws error on second execution')
+  }).on('end',t.end)
+})
+
+
+test('gulp-markdown-equations: completeSync',function(t) {
+  t.plan(4)
+  var eqNum = 0
+  var mdeq = gulpMarkdownEquations(), md
+
+  mdeq.write(new File({ path: 'sample.mdtex', contents: fixtures['sample.mdtex'] }))
+  mdeq.end()
+  mdeq.on('end',t.end)
+
+  mdeq.pipe(tap(function(file) {
+    if( file.extname !== '.md' ) {
+      eqNum++
+      mdeq.completeSync(file,function() {
+        t.equal( this.foo, 'bar', 'this is set to the equation' )
+        return 'inserted-equation-' + eqNum
+      })
+    } else {
+      md = file.contents.toString()
+      t.assert( md.match(/inserted-equation-1/m), 'Markdown contains inserted equations')
+      t.assert( md.match(/inserted-equation-2/m), 'Markdown contains inserted equations')
+    }
+  }))
+})
 
 test('passes a null file',function(t) {
   var numEq = 0, numMd = 0, md
